b6e9071f61ac3b0e0b382c5f092355691a964cb4
Bruno Isau Pedrosa <bruno.isau.pedrosa@gmail.com>
2023-03-29 23:59:46 +1300

    feat(iotdevice): configuring entities for activity and device and their relation

--- iot-tracker/src/iot-device-activity/entities/iot-device-activity.entity.ts
+++ iot-tracker/src/iot-device-activity/entities/iot-device-activity.entity.ts
@@ -1,6 +1,38 @@
 import { BaseEntity } from '../../config/entities/base.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { Expose } from 'class-transformer';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { IotDeviceEntity } from '../../iot-device/entities/iot-device.entity';
 
+@Entity({
+  name: 'IotDeviceActivityEntity',
+  orderBy: {
+    activityDateTime: `DESC`,
+  },
+})
 export class IotDeviceActivityEntity extends BaseEntity {
+  @Expose()
+  @IsBoolean()
+  @Column({
+    type: `boolean`,
+  })
+  public deviceEnabled?: boolean;
 
+  @Expose()
+  @Column({
+    nullable: false,
+    type: 'timestamptz',
+  })
+  public activityDateTime?: Date;
 
+  @Expose()
+  @IsOptional()
+  @ManyToOne(
+    (ty: typeof IotDeviceEntity) => IotDeviceEntity,
+    (device: IotDeviceEntity) => device.activities,
+    {
+      onDelete: 'CASCADE',
+    },
+  )
+  public device?: IotDeviceEntity | null;
 }
--- iot-tracker/src/iot-device/entities/iot-device.entity.ts
+++ iot-tracker/src/iot-device/entities/iot-device.entity.ts
@@ -1,3 +1,43 @@
 import { BaseEntity } from '../../config/entities/base.entity';
+import { Column, Entity, Index, OneToMany } from 'typeorm';
+import { Expose } from 'class-transformer';
+import { IsOptional, IsString } from 'class-validator';
+import { IotDeviceActivityEntity } from '../../iot-device-activity/entities/iot-device-activity.entity';
 
-export class IotDeviceEntity extends BaseEntity {}
+@Entity({
+  name: 'IotDeviceEntity',
+  orderBy: {
+    id: `ASC`,
+  },
+})
+export class IotDeviceEntity extends BaseEntity {
+  @Expose()
+  @IsString()
+  @IsOptional()
+  @Column({
+    nullable: true,
+  })
+  // TODO @bruno type has to be reflect a proper entity or enum in the future
+  public type?: string;
+
+  @Index()
+  @Expose()
+  @IsString()
+  @Column({
+    nullable: true,
+  })
+  @IsOptional()
+  public name: string;
+
+  @Expose()
+  @IsOptional()
+  @OneToMany(
+    (ty: typeof IotDeviceActivityEntity) => IotDeviceActivityEntity,
+    (lineItems: IotDeviceActivityEntity) => lineItems.device,
+    {
+      eager: true,
+      cascade: true,
+      onDelete: 'CASCADE',
+  })
+  public activities?: IotDeviceActivityEntity[];
+}
